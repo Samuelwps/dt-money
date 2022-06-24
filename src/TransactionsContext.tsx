69d0d43fd42bc09d1f024c7071b2a485ca467a04
Samuelwps <samuelrip230@gmail.com>
2022-06-24 17:25:56 -0300

    Finalizing repository transfer

---
+++ src/TransactionsContext.tsx
@@ -0,0 +1,65 @@
+import { createContext, ReactNode, useState, useEffect, useContext  } from "react"
+import { api } from './services/api';
+
+interface TransactionProps{
+    id:number;
+    title: string;
+    amount: number;
+    type:string;
+    category: string;
+    createAt: string;
+}
+
+type TransactionInput = Omit<TransactionProps, "id" | "createAt">
+
+interface TransactionsProviderProps{
+    children:ReactNode;
+}
+
+interface TransactionsContextData {
+  transactions: TransactionProps[];
+  createTransaction: (transactionsInput: TransactionInput) => Promise<void>;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
+
+export function TransactionProvider({children}: TransactionsProviderProps) {
+    const [transactions, setTransactions] = useState<TransactionProps[]>([]);
+
+    useEffect(() => {
+      api.get("transactions")
+          .then(response => setTransactions(response.data.transactions))
+    }, [])
+
+    async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createAt: new Date(),
+    })
+    
+    const { transaction } = response.data;
+
+    console.log(transaction)
+    
+    setTransactions([
+      ...transactions,
+      transaction
+    ]);
+
+    console.log(transactions)
+  }
+
+    return(
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+            {children}
+        </TransactionsContext.Provider>
+    );
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
+}
---
+++ src/components/newTrasactionnModal/index.tsx
@@ -0,0 +1,99 @@
+import { FormEvent, useState } from 'react';
+import Modal from "react-modal"
+import { Container, TrasectionTypeContainer, RadioBox } from "./styled";
+import closeImg from "../../assets/close.svg"
+import incomeImg from "../../assets/income.svg"
+import outcomeImg from "../../assets/outcome.svg"
+import { useContext } from 'react';
+import { TransactionsContext } from '../../TransactionsContext';
+
+interface ModalProps{
+    isOpen:boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose} : ModalProps){
+    const { createTransaction } = useContext(TransactionsContext)
+
+
+    const [type, setType] = useState("deposit")
+    const [title, setTitle] = useState("")
+    const [amount, setAmount] = useState(0)
+    const [category, setCategory] =useState("")
+
+    async function handleClickSubmit(event: FormEvent){
+        event.preventDefault()
+
+        await createTransaction({
+            title,
+            amount,
+            category,
+            type,
+        })
+
+        setTitle("")
+        setType("deposit")
+        setAmount(0)
+        setCategory("")
+        onRequestClose()
+
+    }
+
+
+    return(
+        <Modal isOpen={isOpen} 
+        onRequestClose={onRequestClose} 
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content">
+            <button type="button" onClick={onRequestClose} className="react-modal-close">
+                <img src={closeImg} alt="Fechar modal"/>
+            </button>
+
+            <Container onSubmit={handleClickSubmit}>
+                <h2>Cadastrar transações</h2>
+
+                <input placeholder="Título"
+                value={title}
+                onChange={event => setTitle(event.target.value)}
+                />
+
+                <input placeholder="Valor"  type="number"
+                value={amount}
+                onChange={event => setAmount(Number(event.target.value))}
+                />
+
+                <TrasectionTypeContainer>
+                    <RadioBox
+                        type="button"
+                        onClick={() => { setType("deposit") }}
+                        isActive={type === "deposit"}
+                        activeColor="green"
+
+                    >
+                        <img src={incomeImg} alt="Entrada"/>
+                        <span>Entrada</span>
+                    </RadioBox>
+
+                    <RadioBox
+                        type="button"
+                        onClick={() => { setType("withdraw") }}
+                        isActive={type === "withdraw"}
+                        activeColor="red"
+                    >
+                        <img src={outcomeImg} alt="Saida"/>
+                        <span>Saida</span>
+                    </RadioBox>
+                </TrasectionTypeContainer>
+
+                <input placeholder="Categoria"
+                value={category}
+                onChange={event => setCategory(event.target.value)}
+                />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
+        </Modal>
+    );
+}
